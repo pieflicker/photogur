80a4fa30e83e9e1b9c7ff5bc12a8fc6fa44ed471
pieflicker <nesbitt.greg@gmail.com>
2013-09-04 16:13:55 -0400

    making each picture show at its own url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+
 Photogur::Application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
+  get 'pictures/0' => 'pictures#picture0'
+  get 'pictures/1' => 'pictures#picture1'
+  get 'pictures/2' => 'pictures#picture2'
 
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -59,4 +64,4 @@ Photogur::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
