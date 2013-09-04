41fdad5a445805e95f28348394937c90f9288407
pieflicker <nesbitt.greg@gmail.com>
2013-09-04 16:38:00 -0400

    more refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 
 Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
+  post 'pictures' => "pictures#create" # this is a new line of code
+  get 'pictures/new' => 'pictures#new' # this is also a new line of code
   get 'pictures/:id' => 'pictures#show', as: "picture"
 
 
