2364c23f4db0dfac27224a3d70964c98a42ba750
pieflicker <nesbitt.greg@gmail.com>
2013-09-04 16:21:55 -0400

    refactoring step 4 to use dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 
 Photogur::Application.routes.draw do
-
   get 'pictures' => 'pictures#index'
+  get 'pictures/:id' => 'pictures#show', as: "picture"
 
-  get 'pictures/0' => 'pictures#picture0'
-  get 'pictures/1' => 'pictures#picture1'
-  get 'pictures/2' => 'pictures#picture2'
 
 end
 
